a425721c3b849a949222cfc6ea9f6ceda3ba04ee
Huskiefusion <kddh2005@gmail.com>
2025-12-15 16:20:19 -0800

    added different name tables by gender

--- main.cpp
+++ main.cpp
@@ -5,17 +5,51 @@
 #include <unordered_map>
 #include <set>
 #include <map>
+#include <cassert>
 #include <algorithm>
 #include "nameEntry.hpp"
 
 using namespace std;
 
-struct SortByValue{
-    bool operator()(const pair<char, int>& p1, 
-        const pair<char, int>& p2 ){
-            return p1.second < p2.second;
+void findAvgName(const int& maxNameSize, const vector<unordered_map<char, int>>& letterTable){
+    assert(maxNameSize >= letterTable.size()); // Trying to make a name longer than we have letters for
+    for(int i=0; i < maxNameSize; i++){ 
+        double totalValue=0, total=0;
+        for(auto c: letterTable[i]){
+            totalValue += c.first*c.second;
+            total += c.second;
+        }
+        
+        cout << (char)(totalValue/total) ;
     }
-};
+
+}
+
+void findMedianName(const int& maxNameSize, const vector<unordered_map<char, int>>& letterTable){
+    assert(maxNameSize >= letterTable.size()); // Trying to make a name longer than we have letters for
+    for(int i=0; i < maxNameSize; i++){ 
+        vector<pair<char, int>> letterArr(letterTable[i].begin(), letterTable[i].end());
+        sort(letterArr.begin(), letterArr.end(), [](pair<char, int> &a, pair<char, int> &b){
+            return a.second > b.second;
+        });
+        cout << letterArr[letterArr.size()/2].first;
+    }
+}
+
+void findModeName(const int& maxNameSize, const vector<unordered_map<char, int>>& letterTable){
+    assert(maxNameSize >= letterTable.size()); // Trying to make a name longer than we have letters for
+    
+    for(int i=0; i < maxNameSize; i++){
+        int max=0; char maxLetter='\0';
+        for(auto p: letterTable[i]){
+            if(p.second > max){
+                max=p.second;
+                maxLetter=p.first;
+            }
+        }
+        cout << maxLetter;
+    }
+}
 
 
 int main(){
@@ -26,7 +60,7 @@ int main(){
     vector<NameEntry> maleNameArr(1000); // there are 1000 lines in the csv file
     vector<NameEntry> femaleNameArr(1000); 
     vector<string> fields(5); // for storing fields as we read the csv
-    int maxNameSize = 0; // gets updated in the csv read loop
+    int maxNameSize = 0, maxMaleName=0, maxFemaleName=0; // gets updated in the csv read loop
 
     while(getline(nameFile, line)){ // read the whole csv file
         stringstream lineStream(line);
@@ -48,57 +82,48 @@ int main(){
         }
         catch(exception) {
             throw "stoi() call failed!";
-        }
-
-        // update maxNameSize
-        maxNameSize = (fields[1].size() > maxNameSize) ? fields[1].size():maxNameSize;
-        maxNameSize = (fields[3].size() > maxNameSize) ? fields[3].size():maxNameSize;
+        }        
+        // maxMale
+        maxMaleName = (fields[1].size() > maxMaleName) ? fields[1].size():maxMaleName;
+        //maxFemale
+        maxFemaleName = (fields[3].size() > maxFemaleName) ? fields[3].size():maxFemaleName;
     }
-    
+    maxNameSize = (maxFemaleName > maxMaleName) ? maxFemaleName : maxMaleName;
+    cout << maxNameSize << ", " << maxMaleName <<", "<<maxFemaleName <<  endl;
+
     // the world is my cloyster
-    vector<unordered_map<char, int>> letterTable(maxNameSize); // unordered, not sorted
+    vector<unordered_map<char, int>> letterTable(maxNameSize), 
+    maleTable(maxMaleName), 
+    femaleTable(maxFemaleName);
+    // read male names to hash table (and also the merged table)
     for(auto nameE: maleNameArr){
         for(int i=0; i < nameE.name.size(); i++){
+            maleTable[i][nameE.name[i]]+=nameE.count;
             letterTable[i][nameE.name[i]]+=nameE.count;
         }
     }
+    // read female names to hash table (and also the merged table)
+    for(auto nameE: femaleNameArr){
+        for(int i=0; i < nameE.name.size(); i++){
+            femaleTable[i][nameE.name[i]]+=nameE.count;
+            letterTable[i][nameE.name[i]]+=nameE.count;
+        }
+    }
+    
+
 
     // Mean of male names
     cout << "--- MEAN NAME ---" << endl;
-    for(int i=0; i < maxNameSize; i++){ 
-        double totalValue=0, total=0;
-        for(auto c: letterTable[i]){
-            totalValue += c.first*c.second;
-            total += c.second;
-        }
-        
-        cout << (char)(totalValue/total) ;
-    }
+    findAvgName(maxNameSize, letterTable);
     cout << endl << endl;
 
     //Median of male names
     cout << "--- MEDIAN NAME (sorted by letter popularity) ---" << endl;
-    
-    for(int i=0; i < maxNameSize; i++){ 
-        vector<pair<char, int>> letterArr(letterTable[i].begin(), letterTable[i].end());
-        sort(letterArr.begin(), letterArr.end(), [](pair<char, int> &a, pair<char, int> &b){
-            return a.second > b.second;
-        });
-        cout << letterArr[letterArr.size()/2].first;
-    }
+    findMedianName(maxNameSize, letterTable);
     cout << endl << endl;
     
     cout << "--- MODE NAME ---" << endl;
-    for(int i=0; i < maxNameSize; i++){
-        int max=0; char maxLetter='\0';
-        for(auto p: letterTable[i]){
-            if(p.second > max){
-                max=p.second;
-                maxLetter=p.first;
-            }
-        }
-        cout << maxLetter;
-    }
+    findModeName(maxNameSize, letterTable);
 
     cout << endl << endl;
     return 0;
